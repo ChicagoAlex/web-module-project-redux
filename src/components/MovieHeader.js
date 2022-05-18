eb126e35d9bd7697c7efa40e65dfde93ddb7618e
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-05-18 09:42:10 -0500

    movie list rendered, and movie header functionality built

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import { Link } from 'react-router-dom';
 
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const {appTitle} = props;
     const displayFavorites = true;
     
     return(<div className="table-title">
@@ -17,6 +18,12 @@ const MovieHeader = (props) => {
         </div>
         </div>
     </div>);
+}
+
+const mapStateToProps = (state)=>{
+    return({
+        appTitle: state.appTitle
+    });
 }
 
 export default MovieHeader;
