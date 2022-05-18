922ce192f0c4ccfacf9afb28eed1a7eeda00ce79
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-05-18 10:37:45 -0500

    hide favorites function added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,7 @@ const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const {movies, deleteMovie} = props;
+    const {movies, displayFavorites, deleteMovie} = props;
     const movie = movies.find(movie=>movie.id===Number(id));
 
     const handleDeleteClick = ()=>{
@@ -56,6 +56,7 @@ const Movie = (props) => {
 
 const mapStateToProps = (state)=>{
     return({
+        displayFavorites: state.favoritesReducer.displayFavorites,
         movies:state.movieReducer.movies
     });
 }
--- src/reducers/favoritesReducers.js
+++ src/reducers/favoritesReducers.js
@@ -5,6 +5,24 @@ const initialState = {
 
 const favoritesReducer = (state = initialState, action) =>{
     switch(action.type){
+        case(TOGGLE_FAVORITES):{
+            return{
+                ...state,
+                displayFavorites: !state.displayFavorites
+            }
+        }
+        case(ADD_FAVORITE):{
+            return{
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        }
+        case(REMOVE_FAVORITE):{
+            return{
+                ...state,
+                favorites: state.filter(item=>(item.id !== action.payload))
+            }
+        }
         default:
             return(state);
     }
